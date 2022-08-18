f79c08dfca4e8996a2cf4fce99691025ba4b391d
Raymond <mickey_ye@126.com>
2022-08-18 23:41:47 +0800

    seconde commit

--- src/main/java/com/atguigu/Git/GitTestVersion.java
+++ src/main/java/com/atguigu/Git/GitTestVersion.java
@@ -3,5 +3,6 @@ package com.atguigu.Git;
 public class GitTestVersion {
     public static void main(String[] args) {
         System.out.println("hello world!");
+        System.out.println("version2");
     }
 }
